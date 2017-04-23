951693b13a0ad0ee0fa191acf53fa27a34f82b43
Andrey Sinitsyn <andrey.sin98@gmail.com>
2017-04-23 12:31:02 +0300

    Initial commit

---
+++ src/composeReducers.js
@@ -0,0 +1,17 @@
+import cloneDeep from 'lodash.clonedeep';
+
+export default function composeReducers() {
+    // eslint-disable-next-line
+    const reducers = Array.from(arguments);
+
+  return reducers.reduce(
+        (previousReducer, currentReducer) => (
+            // true to indicate that this function is composed,
+            // so reducers created by createGeneralReducer will not create new state for each and everybody,
+            // only first one, provided by composeReducers will
+            (state, action) =>
+                currentReducer(previousReducer(state, action, true), action, true)
+        ),
+        state => cloneDeep(state)
+    );
+}
---
+++ src/createGeneralReducer.js
@@ -0,0 +1,39 @@
+import cloneDeep from 'lodash.clonedeep';
+
+export default function createGeneralReducer(
+  actionInvocationPredicate = true,
+  // eslint-disable-next-line
+  stateProcessorPredicate = (newState, action) => newState,
+  actionProcessorPredicate = action => action
+) {
+  const shouldActionInvocationPredicateBeInvoked = typeof actionInvocationPredicate === 'function';
+
+  // eslint-disable-next-line
+  return function generalReducer(state, action, __reducio__composed = false) {
+    let reducerTriggered = false;
+
+    if(shouldActionInvocationPredicateBeInvoked)
+      reducerTriggered = actionInvocationPredicate(action);
+    else
+            reducerTriggered = !!actionInvocationPredicate;
+
+    if(reducerTriggered) {
+      let newState;
+
+      // if this reducer is part of composed chain,
+      // then we have no need to worry about cloning state,
+      // since composeReducers does it for us
+
+      // eslint-disable-next-line
+      if(!__reducio__composed)
+        newState = cloneDeep(state);
+      else
+                newState = state;
+      const processedAction = actionProcessorPredicate(action);
+
+      return stateProcessorPredicate(newState, processedAction);
+    }
+
+    return state;
+  };
+}
